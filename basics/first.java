62991450de2c4b03543736b5254b02f5a601b608
cpucode <923992029@qq.com>
2020-07-06 21:14:57 +0800

    第一个程序

---
+++ basics/first.java
@@ -0,0 +1,21 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-07-06 20:22:36
+ * @LastEditTime: 2020-07-06 20:48:09
+ * @FilePath: \java\basics\first.java
+ * @Gitee: https://gitee.com/cpu_code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ */ 
+
+/**
+ * 第一个Java程序
+ * Hello world !
+ */
+public class first
+{
+    public static void main(String[] args) 
+    {
+		
+		System.out.println("Hello world !");
+    }
+}
