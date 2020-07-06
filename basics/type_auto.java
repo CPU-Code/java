1270ad1df3223ac9dd0d62b5c1a9aba21baeba4a
cpucode <923992029@qq.com>
2020-07-06 22:41:42 +0800

    自动类型转换

--- basics/type_auto.java
+++ basics/type_auto.java
@@ -1,11 +1,12 @@
 /*
  * @Author: cpu_code
  * @Date: 2020-07-06 21:41:19
- * @LastEditTime: 2020-07-06 21:42:42
+ * @LastEditTime: 2020-07-06 22:04:48
  * @FilePath: \java\basics\type_auto.java
  * @Gitee: https://gitee.com/cpu_code
  * @CSDN: https://blog.csdn.net/qq_44226094
  */ 
+
 
  /**
  * 赋值运算符中的自动类型转换
