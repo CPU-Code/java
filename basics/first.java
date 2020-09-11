409c9c7e7fd50609ec4635c69b38a86d7691552a
cpucode <923992029@qq.com>
2020-09-11 14:24:31 +0800

    更新注释

--- basics/first.java
+++ basics/first.java
@@ -1,7 +1,7 @@
 /*
  * @Author: cpu_code
  * @Date: 2020-07-06 20:22:36
- * @LastEditTime: 2020-07-06 20:48:09
+ * @LastEditTime: 2020-09-11 11:47:12
  * @FilePath: \java\basics\first.java
  * @Gitee: https://gitee.com/cpu_code
  * @CSDN: https://blog.csdn.net/qq_44226094
@@ -11,11 +11,21 @@
  * 第一个Java程序
  * Hello world !
  */
-public class first
+/* public是访问修饰符，表示该class是公开的 */
+public class first    // 类名是 first
 {
+/*
+类名必须以英文字母开头，后接字母，数字和下划线的组合
+习惯以大写字母开头 */
     public static void main(String[] args) 
     {
-		
-		System.out.println("Hello world !");
-    }
+        // write your code here
+        // 方法代码...
+
+        // 向屏幕输出文本:
+        System.out.println("Hello world !");
+        /* 多行注释开始
+        注释内容
+        注释结束 */
+    }   // 方法定义结束
 }
