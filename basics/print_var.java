67a0a0b4de27e296fb0536b5affc58e84dec7d03
cpucode <923992029@qq.com>
2020-09-11 14:25:14 +0800

    print_var__输出数

---
+++ basics/print_var.java
@@ -0,0 +1,21 @@
+/*
+ * @由于个人水平有限, 难免有些错误, 还请指点:  
+ * @Author: cpu_code
+ * @Date: 2020-09-11 11:43:46
+ * @LastEditTime: 2020-09-11 12:03:16
+ * @FilePath: \java\basics\print_var.java
+ * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
+ * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
+ * @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
+ * @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
+ */
+package basics;
+
+public class print_var {
+    public static void main(String[] args){
+        // 定义了一个整型int类型的变量, 初始值为1
+        int x = 100; // 定义int类型变量x，并赋予初始值100
+        
+        System.out.println(x); // 打印该变量的值
+    }
+}
