8fcc15ee50c6542bbbe46609f3302738c73696ce
cpucode <923992029@qq.com>
2020-09-12 13:31:22 +0800

    nextInt_max__最大值

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @由于个人水平有限, 难免有些错误, 还请指点:  
  * @Author: cpu_code
  * @Date: 2020-07-12 12:03:11
- * @LastEditTime: 2020-09-12 13:24:20
+ * @LastEditTime: 2020-09-12 13:29:18
  * @FilePath: \java\README.md
  * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
  * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
@@ -102,6 +102,7 @@ java编程基础 面向对象 javaAPI 集合 IO GUI JD8C 多线程 网络编程
 - [x] [conversion_char__转化为char](javaAPI/conversion_char.java)
 - [x] [nextInt_int__输入整数](javaAPI/nextInt_int.java)
 - [x] [nextInt_sum__和](javaAPI/nextInt_sum.java)
+- [x] [nextInt_max__最大值](javaAPI/nextInt_max.java)
 
 -----------------------
 
---
+++ javaAPI/nextInt_max.java
@@ -0,0 +1,30 @@
+/*
+ * @由于个人水平有限, 难免有些错误, 还请指点:  
+ * @Author: cpu_code
+ * @Date: 2020-09-12 13:29:43
+ * @LastEditTime: 2020-09-12 13:30:16
+ * @FilePath: \java\javaAPI\nextInt_max.java
+ * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
+ * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
+ * @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
+ * @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
+ */
+package javaAPI;
+
+public class nextInt_max {
+    public static void main(String[] args) {
+        //创建对象
+        Scanner sc = new Scanner(System.in);
+
+        //接收数据
+        System.out.println("输入数字");
+        int a = sc.nextInt();
+
+        System.out.println("输入数据");
+        int b = sc.nextInt();
+
+        // 数据求和
+        int sum = a + b;
+        System.out.println("sum " + sum);
+    }
+}
