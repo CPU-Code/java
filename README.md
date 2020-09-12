873a914e2bd6145bf3abb8d84b3f65105ca1703a
cpucode <923992029@qq.com>
2020-09-12 11:45:00 +0800

    error_string_compare__错误string比较

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <!--
  * @Author: cpu_code
  * @Date: 2020-07-06 20:17:11
- * @LastEditTime: 2020-09-12 11:40:46
+ * @LastEditTime: 2020-09-12 11:44:31
  * @FilePath: \java\README.md
  * @Gitee: https://gitee.com/cpu_code
  * @CSDN: https://blog.csdn.net/qq_44226094
@@ -97,7 +97,7 @@ java编程基础 面向对象 javaAPI 集合 IO GUI JD8C 多线程 网络编程
 ## [javaAPI](javaAPI)
 
 - [x] [string_capital__string转换为大写](javaAPI/string_capital.java)
-- [x] [string_compare__string比较](javaAPI/string_compare.java)
+- [x] [error_string_compare__错误string比较](javaAPI/error_string_compare.java)
 
 -----------------
 
--- javaAPI/error_string_compare.java
+++ javaAPI/error_string_compare.java
@@ -2,8 +2,8 @@
  * @由于个人水平有限, 难免有些错误, 还请指点:  
  * @Author: cpu_code
  * @Date: 2020-09-12 11:41:01
- * @LastEditTime: 2020-09-12 11:43:22
- * @FilePath: \java\javaAPI\string_compare.java
+ * @LastEditTime: 2020-09-12 11:44:56
+ * @FilePath: \java\javaAPI\error_string_compare.java
  * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
  * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
  * @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
@@ -11,11 +11,13 @@
  */
 package javaAPI;
 
-public class string_compare {
+public class error_string_compare {
     public static void main(String[] args) {
         String s1 = "cpucode";
         String s2 = "cpucode";
 
+        // 必须使用equals()方法而不能用==
+        
         System.out.println(s1 == s2);
         System.out.println(s1.equals(s2));
     }
