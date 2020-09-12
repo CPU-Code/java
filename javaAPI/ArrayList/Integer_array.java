e861eab7aa59a098663389d45c7189f0a8ea0b38
cpucode <923992029@qq.com>
2020-09-12 17:08:51 +0800

    Integer_array__Integer集合对象

---
+++ javaAPI/ArrayList/Integer_array.java
@@ -0,0 +1,30 @@
+/*
+ * @由于个人水平有限, 难免有些错误, 还请指点:  
+ * @Author: cpu_code
+ * @Date: 2020-09-12 17:08:07
+ * @LastEditTime: 2020-09-12 17:08:31
+ * @FilePath: \java\javaAPI\ArrayList\Integer_array.java
+ * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
+ * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
+ * @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
+ * @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
+ */
+package javaAPI.ArrayList;
+
+import java.util.ArrayList;
+
+public class Integer_array {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+    }
+}
+
+/*
+[1, 2, 3]
+*/
