75ef535514010eb2d288c7650ae542268cb06124
cpucode <923992029@qq.com>
2020-09-13 15:13:04 +0800

    print_random__随机数

--- javaAPI/Random/print_random.java
+++ javaAPI/Random/print_random.java
@@ -2,34 +2,42 @@
  * @由于个人水平有限, 难免有些错误, 还请指点:  
  * @Author: cpu_code
  * @Date: 2020-09-12 14:19:23
- * @LastEditTime: 2020-09-12 14:19:54
- * @FilePath: \java\javaAPI\print_random.java
+ * @LastEditTime: 2020-09-13 14:44:11
+ * @FilePath: \java\javaAPI\Random\print_random.java
  * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
  * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
  * @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
  * @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
  */
-package javaAPI;
+package javaAPI.Random;
 
 import java.util.Random;
 
+/*
+Random类用来生成随机数字。使用起来也是三个步骤：
+
+1. 导包
+import java.util.Random;
+
+2. 创建
+Random r = new Random(); // 小括号当中留空即可
+
+3. 使用
+获取一个随机的int数字（范围是int所有范围，有正负两种）：int num = r.nextInt()
+获取一个随机的int数字（参数代表了范围，左闭右开区间）：int num = r.nextInt(3)
+实际上代表的含义是：[0,3)，也就是0~2
+ */
+
 public class print_random {
     public static void main(String[] args) {
-        // 创建键盘录入数据的对象
         Random r = new Random();
 
-        for(int i = 0; i < 3; i++) {
-            // 随机生成一个数据
-            // 范围在 0 （包括）和 指定值 n （不包括）之间的 int 值
-            int number = r.nextInt(10);
-            // 输出数据
-            System.out.println("number:" + number);
-        }
+        int num = r.nextInt();
+
+        System.out.println("随机数 :" + num);
     }
 }
 
 /*
-number:8
-number:6
-number:3
+随机数 :-145013416
 */
