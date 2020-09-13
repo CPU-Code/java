2f0df573baba82f57fae77d38dc03cd59893a1d8
cpucode <923992029@qq.com>
2020-09-13 16:02:36 +0800

    extend5__继承成员方法重名重写

--- object/Extends/extend5.java
+++ object/Extends/extend5.java
@@ -2,7 +2,7 @@
  * @由于个人水平有限, 难免有些错误, 还请指点:  
  * @Author: cpu_code
  * @Date: 2020-09-13 15:51:46
- * @LastEditTime: 2020-09-13 15:54:09
+ * @LastEditTime: 2020-09-13 15:55:51
  * @FilePath: \java\object\Extends\extend5.java
  * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
  * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
@@ -10,6 +10,12 @@
  * @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
  */
 package Extends;
+
+
+/*
+方法重写 ：子类中出现与父类一模一样的方法时（返回值类型，方法名和参数列表都相同），会出现覆盖效
+果，也称为重写或者复写。声明不变，重新实现
+*/
 
 public class extend5 {
     public static void main(String[] args){
