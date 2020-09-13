6fb1a9ba330bdf478f4dffdcae7a859a72a158f5
cpucode <923992029@qq.com>
2020-09-13 20:41:47 +0800

    InterfaceDemo2__继承默认方法

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @由于个人水平有限, 难免有些错误, 还请指点:  
  * @Author: cpu_code
  * @Date: 2020-07-12 12:03:11
- * @LastEditTime: 2020-09-13 20:36:41
+ * @LastEditTime: 2020-09-13 20:41:41
  * @FilePath: \java\README.md
  * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
  * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
@@ -113,7 +113,7 @@ java编程基础 面向对象 javaAPI 集合 IO GUI JD8C 多线程 网络编程
 ### [Interface__接口](object/Interface)
 
 - [x] [InterfaceDemo__抽象方法的使用](object/Interface/InterfaceDemo.java)
-- [ ] [InterfaceDemo__抽象方法的使用](object/Interface/InterfaceDemo.java)
+- [x] [InterfaceDemo2__继承默认方法](object/Interface/InterfaceDemo2.java)
 
 ------------------------
 
---
+++ object/Interface/InterfaceDemo2.java
@@ -0,0 +1,37 @@
+/*
+ * @由于个人水平有限, 难免有些错误, 还请指点:  
+ * @Author: cpu_code
+ * @Date: 2020-09-13 20:37:41
+ * @LastEditTime: 2020-09-13 20:41:04
+ * @FilePath: \java\object\Interface\InterfaceDemo2.java
+ * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
+ * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
+ * @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
+ * @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
+ */
+package Interface;
+
+public class InterfaceDemo2 {
+    public static void main(String[] args){
+        // 创建子类对象
+        Animal a = new Animal();
+        // 调用默认方法
+        a.fly();
+    }
+}
+
+
+interface LiveAble{
+    // 继承默认方法
+    public default void fly(){
+        System.out.println("飞呀");
+    }
+}
+
+class Animal implements LiveAble{
+    // 继承，什么都不用写，直接调用
+}
+
+/*
+飞呀
+*/
