9c50eeed3c27569ba51b316926a4c2cbbe5ed790
cpucode <923992029@qq.com>
2020-09-16 19:37:17 +0800

    Generic7__受限泛型

--- Generic/Generic7.java
+++ Generic/Generic7.java
@@ -2,7 +2,7 @@
  * @由于个人水平有限, 难免有些错误, 还请指点:  
  * @Author: cpu_code
  * @Date: 2020-09-16 19:30:20
- * @LastEditTime: 2020-09-16 19:36:24
+ * @LastEditTime: 2020-09-16 19:37:08
  * @FilePath: \java\Generic\Generic7.java
  * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
  * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
@@ -10,6 +10,9 @@
  * @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
  */
 package Generic;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class Generic7 {
     public static void main(String[] args) {
