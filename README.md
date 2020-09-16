74322f27043026d7e18f36b76f12079ec4152df6
cpucode <923992029@qq.com>
2020-09-16 17:47:50 +0800

    Generic1__错误类型转换异常

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @由于个人水平有限, 难免有些错误, 还请指点:  
  * @Author: cpu_code
  * @Date: 2020-07-12 12:03:11
- * @LastEditTime: 2020-09-16 17:35:16
+ * @LastEditTime: 2020-09-16 17:46:57
  * @FilePath: \java\README.md
  * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
  * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
@@ -276,7 +276,9 @@ java编程基础 面向对象 javaAPI 集合 IO GUI JD8C 多线程 网络编程
 
 ------------
 
-## [泛型]()
+## [泛型](Generic)
+
+- [x] [Generic1__错误类型转换异常](Generic/Generic1.java)
 
 -----------------
 
--- aggregation/aggregation4.java
+++ aggregation/aggregation4.java
@@ -2,7 +2,7 @@
  * @由于个人水平有限, 难免有些错误, 还请指点:  
  * @Author: cpu_code
  * @Date: 2020-09-16 17:35:21
- * @LastEditTime: 2020-09-16 17:37:17
+ * @LastEditTime: 2020-09-16 17:38:47
  * @FilePath: \java\aggregation\aggregation4.java
  * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
  * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
@@ -11,13 +11,18 @@
  */
 package aggregation;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class aggregation4 {
     public static void main(String[] args) {
         Collection<String> coll = new ArrayList<String>();
         coll.add("cpucode");
         coll.add("cpu");
 
+        //使用增强for遍历
         for(String s : coll){
+            //接收变量s代表 代表被遍历到的集合元素
             System.out.println(s);
         }
     }
