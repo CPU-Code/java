9f63021fe978b09b6aef8690d725a8d5486844db
cpucode <923992029@qq.com>
2020-09-16 17:51:59 +0800

    Generic2__泛型的引入

--- Generic/Generic2.java
+++ Generic/Generic2.java
@@ -1,9 +1,9 @@
 /*
  * @由于个人水平有限, 难免有些错误, 还请指点:  
  * @Author: cpu_code
- * @Date: 2020-09-16 17:47:09
- * @LastEditTime: 2020-09-16 17:51:43
- * @FilePath: \java\Generic\Generic1.java
+ * @Date: 2020-09-16 17:51:25
+ * @LastEditTime: 2020-09-16 17:51:32
+ * @FilePath: \java\Generic\Generic2.java
  * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
  * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
  * @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
@@ -15,27 +15,22 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
-public class Generic1 {
+public class Generic2 {
     public static void main(String[] args) {
-        Collection coll = new ArrayList();
+        Collection<String> coll = new ArrayList<String>();
 
         coll.add("cpucode");
         coll.add("cpu");
         //由于集合没有做任何限定，任何类型都可以给其中存放
-        coll.add(5);
-
-        Iterator it = coll.iterator();
+        //coll.add(5);
+        // 集合已经明确具体存放的元素类型，那么在使用迭代器的时候，迭代器也同样会知道具体遍历元素类型
+        Iterator<String> it = coll.iterator();
 
         while (it.hasNext()){
-            //需要打印每个字符串的长度,就要把迭代出来的对象转成String类型
-            String str = (String) it.next();
+            String str = it.next();
+
+            //当使用Iterator<String>控制元素类型后，就不需要强转了。获取到的元素直接就是String类型
             System.out.println(str.length());
         }
     }
 }
-
-/*
-7
-3
-Exception in thread "main" java.lang.ClassCastException: c
-*/
