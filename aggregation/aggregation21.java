f093db8a80fe89bca5f823ded7f161038bc22115
cpucode <923992029@qq.com>
2020-09-17 19:18:30 +0800

    aggregation21__Map集合练习一个字符串中每个字符出现次数

--- aggregation/aggregation21.java
+++ aggregation/aggregation21.java
@@ -2,7 +2,7 @@
  * @由于个人水平有限, 难免有些错误, 还请指点:  
  * @Author: cpu_code
  * @Date: 2020-09-17 18:14:33
- * @LastEditTime: 2020-09-17 18:19:46
+ * @LastEditTime: 2020-09-17 19:16:23
  * @FilePath: \java\aggregation\aggregation21.java
  * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
  * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
@@ -16,24 +16,33 @@ import java.util.HashMap;
 
 public class aggregation21 {
     public static void main(String[] args) {
+        //友情提示
         System.out.println("输入字符串");
 
         String line = new Scanner(System.in).nextLine();
 
+        // 定义 每个字符出现次数的方法
         findChar(line);
     }
 
     private static void findChar(String line){
+        //1:创建一个集合 存储 字符 以及其出现的次数
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
 
+        //2:遍历字符串
         for (int i = 0; i < line.length(); i++){
             char c = line.charAt(i);
 
+            //判断 该字符 是否在键集中
             if(!map.containsKey(c)){
+                //说明这个字符没有出现过
+                //那就是第一次
                 map.put(c, 1);
             } else {
+                //先获取之前的次数
                 Integer count = map.get(c);
 
+                //再次存入 更新
                 map.put(c, ++count);
             }
         }
