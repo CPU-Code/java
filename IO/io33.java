b06bba326f983f99631ccb9d34e70f92b2edc71c
cpucode <923992029@qq.com>
2020-09-19 15:23:46 +0800

    io33__IO异常的处理JDK9的改进

---
+++ IO/io33.java
@@ -0,0 +1,41 @@
+/*
+ * @由于个人水平有限, 难免有些错误, 还请指点:  
+ * @Author: cpu_code
+ * @Date: 2020-09-19 15:19:20
+ * @LastEditTime: 2020-09-19 15:23:39
+ * @FilePath: \java\IO\io33.java
+ * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
+ * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
+ * @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
+ * @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
+ */
+package IO;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class io33 {
+    public static void main(String[] args) throws IOException {
+        // 创建流对象
+        final FileReader fr = new FileReader("cpucode.txt");
+        FileWriter fw = new FileWriter("cpu.txt");
+
+        // 引入到try中
+        try (fr; fw){
+            // 定义变量
+            int b;
+            // 读取数据
+            while ((b = fr.read()) != -1){
+                // 写出数据
+                fw.write(b);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+/*
+cpucode好
+*/
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @由于个人水平有限, 难免有些错误, 还请指点:  
  * @Author: cpu_code
  * @Date: 2020-07-12 12:03:11
- * @LastEditTime: 2020-09-19 15:15:29
+ * @LastEditTime: 2020-09-19 15:19:16
  * @FilePath: \java\README.md
  * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
  * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
@@ -348,6 +348,7 @@ java编程基础 面向对象 javaAPI 集合 IO GUI JD8C 多线程 网络编程
 - [x] [io30__续写和换行](IO/io30.java)
 - [x] [io31__IO异常的处理JDK7前处理](IO/io31.java)
 - [x] [io32__IO异常的处理JDK7的处理](IO/io32.java)
+- [x] [io33__IO异常的处理JDK9的改进](IO/io33.java)
 
 --------------
 
