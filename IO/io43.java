8e3bd7f02a442e0065a9a5277bd54d43a0406f21
cpucode <923992029@qq.com>
2020-09-20 17:23:38 +0800

    io43__指定编码读取

--- IO/io43.java
+++ IO/io43.java
@@ -2,7 +2,7 @@
  * @由于个人水平有限, 难免有些错误, 还请指点:  
  * @Author: cpu_code
  * @Date: 2020-09-20 17:11:01
- * @LastEditTime: 2020-09-20 17:22:40
+ * @LastEditTime: 2020-09-20 17:23:27
  * @FilePath: \java\IO\io43.java
  * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
  * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
@@ -17,14 +17,16 @@ import java.io.InputStreamReader;
 
 public class io43 {
     public static void main(String[] args) throws IOException {
+        // 定义文件路径,文件为gbk编码
         String FileName = "cpucode_gbk.txt";
-
+        // 创建流对象,默认UTF8编码
         InputStreamReader isr = new InputStreamReader(new FileInputStream(FileName));
-
+        // 创建流对象,指定GBK编码
         InputStreamReader isr2 = new InputStreamReader(new FileInputStream(FileName), "GBK");
-
+        // 定义变量,保存字符
         int read;
 
+        // 使用默认编码字符流读取,乱码
         while ((read = isr.read()) != -1) {
             System.out.print((char)read);
         }
@@ -33,6 +35,7 @@ public class io43 {
 
         System.out.println();
 
+        // 使用指定编码字符流读取,正常解析
         while ((read = isr2.read()) != -1) {
             System.out.print((char)read);
         }
