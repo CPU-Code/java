61430c5006d85148a7073251946c227133d3973c
cpucode <923992029@qq.com>
2020-09-20 20:14:26 +0800

    io49__改变打印流向

--- IO/io49.java
+++ IO/io49.java
@@ -2,7 +2,7 @@
  * @由于个人水平有限, 难免有些错误, 还请指点:  
  * @Author: cpu_code
  * @Date: 2020-09-20 20:11:44
- * @LastEditTime: 2020-09-20 20:13:14
+ * @LastEditTime: 2020-09-20 20:14:17
  * @FilePath: \java\IO\io49.java
  * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
  * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
@@ -16,12 +16,15 @@ import java.io.PrintStream;
 
 public class io49 {
     public static void main(String[] args) throws IOException {
+        // 调用系统的打印流,控制台直接输出
         System.out.println("cpucode");
 
+        // 创建打印流,指定文件的名称
         PrintStream ps = new PrintStream("cpucode_out.txt");
-
+        // 设置系统的打印流流向,输出到ps.txt
         System.setOut(ps);
 
+        // 调用系统的打印流,ps.txt中输出
         System.out.println("cpucode");
     }
 }
