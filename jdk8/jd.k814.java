c474b1d1ab6541ff4ffc4e552e5f900014940e7d
cpucode <923992029@qq.com>
2020-09-21 21:41:12 +0800

    jdk814__Predicate接口_默认方法or

---
+++ jdk8/jd.k814.java
@@ -0,0 +1,29 @@
+/*
+ * @由于个人水平有限, 难免有些错误, 还请指点:  
+ * @Author: cpu_code
+ * @Date: 2020-09-21 21:39:59
+ * @LastEditTime: 2020-09-21 21:40:50
+ * @FilePath: \java\jdk8\jd.k814.java
+ * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
+ * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
+ * @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
+ * @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
+ */
+package jdk8;
+
+import java.util.function.Predicate;
+
+public class jd.k814 {
+    public static void main(String[] args) {
+        method(s -> s.contains("c"), s -> s.contains("p"));
+    }
+
+    private static void method(Predicate<String> one, Predicate<String> two) {
+        boolean isValid = one.or(two).test("cpucode");
+        System.out.println("字符串符合不 " + isValid);
+    }
+}
+
+/*
+字符串符合不 true
+*/
