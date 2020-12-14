adc6bd707a07f766fb1f5dc39865aa58eb2f3d7b
cpucode <923992029@qq.com>
2020-12-14 15:21:16 +0800

    Delete__删除数据

---
+++ JDBC/src/jdbc/Delete.java
@@ -0,0 +1,63 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Delete {
+    public static void main(String[] args){
+        Statement statement = null;
+        Connection connection = null;
+/*
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+*/
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql:///cpucode?serverTimezone=UTC",
+                    "root", "12345");
+
+            String sql = "delete from student where id = 3";
+
+            statement = connection.createStatement();
+
+            int count = statement.executeUpdate(sql);
+
+            System.out.println("count = " + count);
+
+            if (count > 0){
+                System.out.println("删除成功！");
+            } else{
+                System.out.println("删除失败!!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally{
+            //7.释放资源
+            if (statement != null){
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
+/*
+count = 1
+删除成功!
+* */
--- README.md
+++ README.md
@@ -460,6 +460,7 @@ java编程基础 面向对象 javaAPI 集合 IO GUI JD8C 多线程 网络编程
 - [x] [First_jdbc__第一jdbc](JDBC/src/jdbc/First_jdbc.java)
 - [x] [Insert__添加数据](JDBC/src/jdbc/Insert.java)
 - [x] [Update__修改数据](JDBC/src/jdbc/Update.java)
+- [x] [Delete__删除数据](JDBC/src/jdbc/Delete.java)
 
 - [返回目录](#文件目录)
 
