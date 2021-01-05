682ca3d4c5b1b14d322ba2dd3afb0b4f7e72fe67
cpucode <923992029@qq.com>
2021-01-05 17:27:14 +0800

    HeapInstanceTest__查看各区数据转换

---
+++ JVM/src/com/cpucode/java/heap/HeapInstanceTest.java
@@ -0,0 +1,40 @@
+/**
+ * User: cpucode
+ * Date: 2021/1/5
+ * Time: 17:13
+ * Github: https://github.com/CPU-Code
+ * CSDN: https://blog.csdn.net/qq_44226094
+ */
+
+package com.cpucode.java.heap;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/*
+
+ -Xms600m -Xmx600m
+通过 visualVM GC发现
+当 Eden space 满的时候 会触发一次 GC
+把数据未回收的数据存放在 survivor 0, 1 俩个其中一个, 反复调换数据位置
+当survivor 满时, 就会把数据存放在Old Gen 中
+当Old Gen 数据满了 , 就会报错OutOfMemoryError
+
+* */
+public class HeapInstanceTest {
+    byte[] buffer = new byte[new Random().nextInt(1024 * 1024)];
+
+    public static void main(String[] args) {
+        ArrayList<HeapInstanceTest> list = new ArrayList<HeapInstanceTest>();
+
+        while(true){
+            list.add(new HeapInstanceTest());
+
+            try{
+                Thread.sleep(10);
+            } catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
