d55efe80711be1c64be5ed1c04e1bac56eaef54a
cpucode <923992029@qq.com>
2021-01-16 19:14:43 +0800

    Bc81__学好c++

---
+++ gettingStarted/src/com/cpucode/java/getting/started/Bc81.java
@@ -0,0 +1,27 @@
+package com.cpucode.java.getting.started;
+
+/**
+ * 题目描述
+ * C++是带类的C语言，这个“++”主要包含三部分内容：对C语言进行语法上的扩展、面向对象（封装、继承和多态），
+ * STL(即模板)。这学期你将学习C++，学好C++，其他编程语言都可以很轻松掌握。
+ *
+ * 输入描述:
+ * 无
+ * 输出描述:
+ * I will learn C++ well!
+ *
+ * @author : cpucode
+ * @Date : 2021/1/16
+ * @Time : 19:12
+ * @Github : https://github.com/CPU-Code
+ * @CSDN : https://blog.csdn.net/qq_44226094
+ */
+public class Bc81 {
+    public static void main(String[] args){
+        test1();
+    }
+
+    private static void test1(){
+        System.out.println("I will learn C++ well!");
+    }
+}
