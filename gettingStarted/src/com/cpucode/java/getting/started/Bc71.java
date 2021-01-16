c741fb1373b412e9a6aff42e1627cf7448030267
cpucode <923992029@qq.com>
2021-01-16 18:54:48 +0800

    Bc71__新年快乐

---
+++ gettingStarted/src/com/cpucode/java/getting/started/Bc71.java
@@ -0,0 +1,21 @@
+package com.cpucode.java.getting.started;
+
+/**
+ * 题目描述
+ * 新年到来，输出信息迎接新年。
+ * 输入描述:
+ * 无
+ * 输出描述:
+ * Happy New Year*2019*
+ *
+ * @author : cpucode
+ * @Date : 2021/1/16
+ * @Time : 18:01
+ * @Github : https://github.com/CPU-Code
+ * @CSDN : https://blog.csdn.net/qq_44226094
+ */
+public class Bc71 {
+    public static void main(String[] args){
+        System.out.println("Happy New Year*2019*");
+    }
+}
