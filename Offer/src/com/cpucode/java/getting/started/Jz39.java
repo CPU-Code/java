4385a8e103c1d64b6031e120e2ce298673ec93cc
cpucode <923992029@qq.com>
2021-01-23 13:48:54 +0800

    未实现

---
+++ Offer/src/com/cpucode/java/getting/started/Jz39.java
@@ -0,0 +1,23 @@
+package com.cpucode.java.getting.started;
+
+/**
+ * 题目描述
+ * 输入一棵二叉树，判断该二叉树是否是平衡二叉树。
+ * 在这里，我们只需要考虑其平衡性，不需要考虑其是不是排序二叉树
+ * 平衡二叉树（Balanced Binary Tree），具有以下性质：
+ * 它是一棵空树或它的左右两个子树的高度差的绝对值不超过1，并且左右两个子树都是一棵平衡二叉树。
+ *
+ * 示例1
+ * 输入
+ * {1,2,3,4,5,6,7}
+ * 返回值
+ * true
+ *
+ * @author : cpucode
+ * @Date : 2021/1/23
+ * @Time : 12:47
+ * @Github : https://github.com/CPU-Code
+ * @CSDN : https://blog.csdn.net/qq_44226094
+ */
+public class Jz39 {
+}
---
+++ gettingStarted/src/com/cpucode/java/simple/Bc47.java
@@ -0,0 +1,25 @@
+package com.cpucode.java.simple;
+
+/**
+ * 题目描述
+ * KiKi想判断输入的字符是不是字母，请帮他编程实现。
+ *
+ * 输入描述:
+ * 多组输入，每一行输入一个字符。
+ * 输出描述:
+ * 针对每组输入，输出单独占一行，判断输入字符是否为字母，输出内容详见输出样例。
+ * 示例1
+ * 输入
+ * A
+ * 6
+ * 输出
+ * A is an alphabet.
+ *
+ * @author : cpucode
+ * @Date : 2021/1/22
+ * @Time : 13:52
+ * @Github : https://github.com/CPU-Code
+ * @CSDN : https://blog.csdn.net/qq_44226094
+ */
+public class Bc47 {
+}
