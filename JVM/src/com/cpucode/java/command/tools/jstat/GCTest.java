40a2c16de1cb7d315ae0d11ea0776a7140572695
cpucode <923992029@qq.com>
2021-02-05 22:04:41 +0800

    GCTest__jstat测试

---
+++ JVM/src/com/cpucode/java/command/tools/jstat/GCTest.java
@@ -0,0 +1,31 @@
+package com.cpucode.java.command.tools.jstat;
+
+import java.util.ArrayList;
+
+/**
+ * -Xms60m -Xmx60m -XX:SurvivorRatio=8
+ *
+ * @author : cpucode
+ * @date : 2021/2/5
+ * @time : 20:28
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+public class GCTest {
+    public static void main(String[] args) {
+        ArrayList<byte[]> list = new ArrayList<>();
+        int num = 1000;
+
+        for (int i = 0; i < num; i++) {
+            //100KB
+            byte[] arr = new byte[1024 * 100];
+            list.add(arr);
+
+            try{
+                Thread.sleep(100);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
