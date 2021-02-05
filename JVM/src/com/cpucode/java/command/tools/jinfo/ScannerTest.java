40527ce03ac2948a66358612a78e38f39e52b1e9
cpucode <923992029@qq.com>
2021-02-05 23:23:09 +0800

    ScannerTest__jinfo测试

---
+++ JVM/src/com/cpucode/java/command/tools/jinfo/ScannerTest.java
@@ -0,0 +1,21 @@
+package com.cpucode.java.command.tools.jinfo;
+
+import java.util.Scanner;
+
+/**
+ * VM options: op-Xms100m -Xms100m
+ * Program arguments: cpuCode
+ *
+ * @author : cpucode
+ * @date : 2021/2/5
+ * @time : 18:42
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+public class ScannerTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String info = scanner.next();
+    }
+}
