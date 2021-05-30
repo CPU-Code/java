f2d5ac61ecdc8a527abaaa641a68d8fb880226d2
cpuCode <923992029@qq.com>
2021-05-30 16:41:50 +0800

    JDK手写实现

---
+++ pattern/src/main/java/com/cpucode/pattern/proxy/dynamicproxy/gpproxy/GPMeipo.java
@@ -0,0 +1,40 @@
+package com.cpucode.pattern.proxy.dynamicproxy.gpproxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author : cpucode
+ * @date : 2021/5/30
+ * @time : 14:28
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+public class GPMeipo implements GPInvocationHandler{
+    private Object target;
+
+    public Object getInstance(Object target) throws Exception{
+        this.target = target;
+        Class<?> clazz = target.getClass();
+
+        return GPProxy.newProxyInstance(new GPClassLoader(),clazz.getInterfaces(),this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        before();
+
+        Object obj = method.invoke(this.target, args);
+
+        after();
+        return obj;
+    }
+
+    private void before(){
+        System.out.println("我是媒婆，我要给你找对象，现在已经确认你的需求");
+        System.out.println("开始物色");
+    }
+
+    private void after(){
+        System.out.println("OK的话，准备办事");
+    }
+}
