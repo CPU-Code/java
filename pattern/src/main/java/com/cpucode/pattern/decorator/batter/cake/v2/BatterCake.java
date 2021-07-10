c898730122e8153489363eb893b444416404346d
cpuCode <923992029@qq.com>
2021-07-10 23:49:26 +0800

    煎饼装饰v2

---
+++ pattern/src/main/java/com/cpucode/pattern/decorator/batter/cake/v2/BatterCake.java
@@ -0,0 +1,13 @@
+package com.cpucode.pattern.decorator.batter.cake.v2;
+
+/**
+ * @author : cpucode
+ * @date : 2021/7/10
+ * @time : 23:36
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+public abstract class BatterCake {
+    protected abstract String getMsg();
+    protected abstract int getPrice();
+}
