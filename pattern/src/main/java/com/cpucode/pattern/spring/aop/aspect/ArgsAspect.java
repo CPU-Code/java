a21528a32357135808fca13d5d46bf12bfbea5d8
cpuCode <923992029@qq.com>
2021-07-13 22:49:47 +0800

    调整一些注释细节

--- pattern/src/main/java/com/cpucode/pattern/spring/aop/aspect/ArgsAspect.java
+++ pattern/src/main/java/com/cpucode/pattern/spring/aop/aspect/ArgsAspect.java
@@ -29,19 +29,19 @@ public class ArgsAspect {
     public void aspect(){	}
 
     /**
-     * 配置前置通知,拦截返回值为cn.ysh.studio.spring.mvc.bean.User的方法
+     * 配置前置通知,拦截返回值为 的方法
      * @param joinPoint
      */
-    @Before("execution(com.gupaoedu.vip.pattern.spring.aop.model.Member com.cpucode.pattern.spring.aop.service..*(..))")
+    @Before("execution(com.cpucode.pattern.spring.aop.model.Member com.cpucode.pattern.spring.aop.service..*(..))")
     public void beforeReturnUser(JoinPoint joinPoint){
         log.info("beforeReturnUser " + joinPoint);
     }
 
     /**
-     * 配置前置通知,拦截参数为cn.ysh.studio.spring.mvc.bean.User的方法
+     * 配置前置通知,拦截参数为 的方法
      * @param joinPoint
      */
-    @Before("execution(* com.cpucode.pattern.spring.aop.service..*(com.gupaoedu.vip.pattern.spring.aop.model.Member))")
+    @Before("execution(* com.cpucode.pattern.spring.aop.service..*(com.cpucode.pattern.spring.aop.model.Member))")
     public void beforeArgUser(JoinPoint joinPoint){
         log.info("beforeArgUser " + joinPoint);
     }
@@ -51,7 +51,7 @@ public class ArgsAspect {
      * @param joinPoint
      * @param id
      */
-    @Before("aspect()&&args(id)")
+    @Before("aspect() && args(id)")
     public void beforeArgId(JoinPoint joinPoint, long id){
         log.info("beforeArgId " + joinPoint + "\tID:" + id);
     }
--- pattern/src/main/java/com/cpucode/pattern/spring/aop/aspect/XmlAspect.java
+++ pattern/src/main/java/com/cpucode/pattern/spring/aop/aspect/XmlAspect.java
@@ -2,70 +2,82 @@ package com.cpucode.pattern.spring.aop.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.stereotype.Component;
+import org.aspectj.lang.ProceedingJoinPoint;
 
 /**
- * 获取参数Aspect切面Bean
+ * XML版Aspect切面Bean
  *
- * @Component : 声明这是一个组件
- * @Aspect : 声明这是一个切面Bean
  * @author : cpucode
  * @date : 2021/7/13
  * @time : 22:12
  * @github : https://github.com/CPU-Code
  * @csdn : https://blog.csdn.net/qq_44226094
  */
-
-@Component
-@Aspect
 @Slf4j
 public class XmlAspect {
 
-
-
-
-
-    /*
-     * 配置前置通知,使用在方法aspect()上注册的切入点
-     * 同时接受JoinPoint切入点对象,可以没有该参数
+    /**
+     * 配置前置通知,使用在方法 aspect()上注册的切入点
+     * 同时接受 JoinPoint 切入点对象,可以没有该参数
      */
     public void before(JoinPoint joinPoint){
-        log.info("" + joinPoint.getArgs()); //获取实参列表
-        log.info("" + joinPoint.getKind());	//连接点类型，如method-execution
-        log.info("" + joinPoint.getSignature()); //获取被调用的切点
-        log.info("" + joinPoint.getTarget());	//获取目标对象
-        log.info("" + joinPoint.getThis());	//获取this的值
+        //获取实参列表
+        log.info("" + joinPoint.getArgs());
+        //连接点类型，如method-execution
+        log.info("" + joinPoint.getKind());
+        //获取被调用的切点
+        log.info("" + joinPoint.getSignature());
+        //获取目标对象
+        log.info("" + joinPoint.getTarget());
+        //获取this的值
+        log.info("" + joinPoint.getThis());
 
         log.info("before通知 " + joinPoint);
     }
 
-    //配置后置通知,使用在方法aspect()上注册的切入点
+    /**
+     * 配置后置通知,使用在方法aspect()上注册的切入点
+     * @param joinPoint
+     */
     public void after(JoinPoint joinPoint){
         log.info("after通知 " + joinPoint);
     }
 
-    //配置环绕通知,使用在方法aspect()上注册的切入点
+    /**
+     * 配置环绕通知,使用在方法aspect()上注册的切入点
+     * @param joinPoint
+     */
     public void around(JoinPoint joinPoint){
         long start = System.currentTimeMillis();
+
         try {
             ((ProceedingJoinPoint) joinPoint).proceed();
             long end = System.currentTimeMillis();
-            log.info("around通知 " + joinPoint + "\tUse time : " + (end - start) + " ms!");
+
+            log.info("around通知 " + joinPoint +
+                    "\tUse time : " + (end - start) + " ms!");
         } catch (Throwable e) {
             long end = System.currentTimeMillis();
-            log.info("around通知 " + joinPoint + "\tUse time : " + (end - start) + " ms with exception : " + e.getMessage());
+
+            log.info("around通知 " + joinPoint +
+                    "\tUse time : " + (end - start) +
+                    " ms with exception : " + e.getMessage());
         }
     }
 
-    //配置后置返回通知,使用在方法aspect()上注册的切入点
+    /**
+     * 配置后置返回通知,使用在方法aspect()上注册的切入点
+     * @param joinPoint
+     */
     public void afterReturn(JoinPoint joinPoint){
         log.info("afterReturn通知 " + joinPoint);
     }
 
-    //配置抛出异常后通知,使用在方法aspect()上注册的切入点
+    /**
+     * 配置抛出异常后通知,使用在方法aspect()上注册的切入点
+     * @param joinPoint
+     * @param ex
+     */
     public void afterThrow(JoinPoint joinPoint, Exception ex){
         log.info("afterThrow通知 " + joinPoint + "\t" + ex.getMessage());
     }
