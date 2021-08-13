738b8efe1f62faf997f80d8a34d5e2d185a0112a
cpuCode <923992029@qq.com>
2021-08-13 14:27:43 +0800

    聊天服务端

--- IO/src/main/java/com/cpucode/java/netty/io/nio/chat/NioChatServer.java
+++ IO/src/main/java/com/cpucode/java/netty/io/nio/chat/NioChatServer.java
@@ -4,10 +4,8 @@ import com.sun.org.apache.bcel.internal.generic.Select;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
 import java.nio.charset.Charset;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -42,11 +40,6 @@ public class NioChatServer {
 
     private Selector selector = null;
 
-    public static void main(String[] args) throws IOException {
-        new NioChatServer(8080).listen();
-    }
-
-
     public NioChatServer(int port) throws IOException {
         this.port = port;
         ServerSocketChannel server = ServerSocketChannel.open();
@@ -58,6 +51,10 @@ public class NioChatServer {
         server.register(selector, SelectionKey.OP_ACCEPT);
 
         System.out.println("服务已启动，监听端口是：" + this.port);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new NioChatServer(8080).listen();
     }
 
 
@@ -77,6 +74,7 @@ public class NioChatServer {
             while(iterator.hasNext()) {
                 SelectionKey key = (SelectionKey) iterator.next();
                 iterator.remove();
+
                 process(key);
             }
         }
@@ -102,29 +100,82 @@ public class NioChatServer {
         //处理来自客户端的数据读取请求
         if (key.isReadable()) {
             //返回该SelectionKey对应的 Channel，其中有数据需要读取
+            SocketChannel client = (SocketChannel)key.channel();
 
+            ByteBuffer buff = ByteBuffer.allocate(1024);
+            StringBuilder content = new StringBuilder();
+            try{
+                while (client.read(buff) > 0){
+                    buff.flip();
+                    content.append(charset.decode(buff));
+                }
+
+                //将此对应的channel设置为准备下一次接受数据
+                key.interestOps(SelectionKey.OP_READ);
+            }catch (IOException io){
+                key.cancel();
+                if(key.channel() != null) {
+                    key.channel().close();
+                }
+            }
             if(content.length() > 0) {
+                String[] arrayContent = content.toString().split(USER_CONTENT_SPILIT);
 
                 //注册用户
                 if(arrayContent != null && arrayContent.length == 1) {
+                    String nickName = arrayContent[0];
+                    if (users.contains(nickName)){
+                        client.write(charset.encode(USER_EXIST));
+                    }else {
+                        users.add(nickName);
+                        int onlineCount = onlineCount();
+                        String message = "欢迎" + nickName +
+                                "进入聊天室! 当前在线人数:" + onlineCount;
 
+                        broadCast(null, message);
+                    }
                 }
                 //注册完了，发送消息
-                else if (){
+                else if (arrayContent != null && arrayContent.length > 1){
+                    String nickName = arrayContent[0];
+                    String message = content.substring(nickName.length() + USER_CONTENT_SPILIT.length());
 
+                    message = nickName + "说 " + message;
+                    if (users.contains(nickName)){
+                        //不回发给发送此内容的客户端
+                        broadCast(client, message);
+                    }
                 }
             }
         }
 
     }
 
-
     public int onlineCount() {
+        int res = 0;
 
+        for(SelectionKey key : selector.keys()){
+            Channel target = key.channel();
+
+            if (target instanceof SocketChannel){
+                res++;
+            }
+        }
+
+        return res;
     }
 
     public void broadCast(SocketChannel client, String content) throws IOException {
+        //广播数据到所有的SocketChannel中
+        for (SelectionKey key : selector.keys()){
+            Channel targetChannel = key.channel();
 
+            //如果client不为空，不回发给发送此内容的客户端
+            if (targetChannel instanceof SocketChannel && targetChannel != client){
+                SocketChannel target = (SocketChannel)targetChannel;
+                target.write(charset.encode(content));
+            }
+        }
     }
 
 }
