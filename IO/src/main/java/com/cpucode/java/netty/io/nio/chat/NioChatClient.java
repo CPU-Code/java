21babd9d4d7d42f0e7cbda0a95407a803973a14c
cpuCode <923992029@qq.com>
2021-08-13 14:27:34 +0800

    聊天客户端

--- IO/src/main/java/com/cpucode/java/netty/io/nio/chat/NioChatClient.java
+++ IO/src/main/java/com/cpucode/java/netty/io/nio/chat/NioChatClient.java
@@ -1,10 +1,17 @@
 package com.cpucode.java.netty.io.nio.chat;
 
+import com.sun.xml.internal.ws.api.client.SelectOptimalEncodingFeature;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
 
 /**
  * @author : cpucode
@@ -13,7 +20,8 @@ import java.nio.charset.Charset;
  * @csdn : https://blog.csdn.net/qq_44226094
  */
 public class NioChatClient {
-    private final InetSocketAddress serverAdrress = new InetSocketAddress("localhost", 8080);
+    private final InetSocketAddress serverAdrress =
+            new InetSocketAddress("localhost", 8080);
     private Selector selector = null;
     private SocketChannel client = null;
 
@@ -21,6 +29,14 @@ public class NioChatClient {
     private Charset charset = Charset.forName("UTF-8");
     private static String USER_EXIST = "系统提示：该昵称已经存在，请换一个昵称";
     private static String USER_CONTENT_SPILIT = "#@#";
+
+    public NioChatClient() throws IOException{
+        selector = Selector.open();
+        //连接远程主机的IP和端口
+        client = SocketChannel.open(serverAdrress);
+        client.configureBlocking(false);
+        client.register(selector, SelectionKey.OP_READ);
+    }
 
     public static void main(String[] args) throws IOException {
         new NioChatClient().session();
@@ -32,4 +48,86 @@ public class NioChatClient {
         //开辟一个新线程往服务器端写数据
         new Writer().start();
     }
+
+    private class Writer extends Thread{
+        @Override
+        public void run() {
+            try{
+                //在主线程中 从键盘读取数据输入到服务器端
+                Scanner scan = new Scanner(System.in);
+                while (scan.hasNextLine()){
+                    String line = scan.nextLine();
+                    if ("".equals(line)){
+                        //不允许发空消息
+                        continue;
+                    }
+
+                    if ("".equals(nickName)){
+                        nickName = line;
+                        line = nickName + USER_CONTENT_SPILIT;
+                    }else {
+                        line = nickName + USER_CONTENT_SPILIT + line;
+                    }
+
+                    //client既能写也能读，这边是写
+                    client.write(charset.encode(line));
+                }
+                scan.close();
+            }catch(Exception e){
+
+            }
+        }
+    }
+
+    private class Reader extends Thread {
+        @Override
+        public void run() {
+            try{
+                while(true) {
+                    int readyChannels = selector.select();
+                    if (readyChannels == 0){
+                        continue;
+                    }
+
+                    //可以通过这个方法，知道可用通道的集合
+                    Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                    Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+
+                    while (keyIterator.hasNext()){
+                        SelectionKey key = (SelectionKey) keyIterator.next();
+                        keyIterator.remove();
+                        process(key);
+                    }
+                }
+            }catch(Exception e){
+
+            }
+        }
+
+        private void process(SelectionKey key) throws IOException {
+            if (key.isReadable()){
+                //使用 NIOServerDemoBak 读取 Channel 中的数据，这个和全局变量client是一样的，
+                // 因为只注册了一个SocketChannel , client既能写也能读，这边是读
+                SocketChannel sc = (SocketChannel)key.channel();
+
+                ByteBuffer buff = ByteBuffer.allocate(1024);
+                String content = "";
+
+                while (sc.read(buff) > 0){
+                    buff.flip();
+                    content += charset.decode(buff);
+                }
+
+                //若系统发送通知名字已经存在，则需要换个昵称
+                if (USER_EXIST.equals(content)){
+                    nickName = "";
+                }
+
+                System.out.println(content);
+                key.interestOps(SelectionKey.OP_READ);
+            }
+        }
+    }
+
+
 }
