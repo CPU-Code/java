d4e397c0e4412eecfe8f43d01b9d2b18b2c3269c
cpuCode <923992029@qq.com>
2021-12-21 08:43:51 +0800

    加法

---
+++ mathNum/README.md
@@ -0,0 +1,15 @@
+
+# [数学运算](src/main/java/com/cpucode/mathNum)
+
+## 文件目录
+
+- [](#)
+
+-----------
+
+## [数学运算](src/main/java/com/cpucode/mathNum)
+
+- [x] [加法](src/main/java/com/cpucode/mathNum/AddTest.java)
+
+
+- [返回目录](../README.md)
---
+++ mathNum/src/main/java/com/cpucode/mathNum/AddTest.java
@@ -0,0 +1,18 @@
+package com.cpucode.mathNum;
+
+/**
+ * 使x能进行加运算，所以要对x进行拆箱
+ *
+ * @author : cpucode
+ * @date : 2021/12/21 8:40
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+public class AddTest {
+    public static void main(String[] args) {
+        Integer x = 5;
+        x = x + 10;
+
+        System.out.println(x);
+    }
+}
